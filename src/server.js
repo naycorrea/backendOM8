0804867d096203f6720b1fb8f20856421047d43d
nayaracorrea <43212442+nayaracorrea@users.noreply.github.com>
2020-05-08 19:03:50 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://nayara:190993@cluster0-8doqy.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+server.use(cors());
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
